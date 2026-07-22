caa75aee65c14f8a7c8e73184d9d8c9b1045a696
khushi <khushimishra1082002@gmail.com>
2026-07-22 04:54:33 -0700

    add changes

--- src/socket.js
+++ src/socket.js
@@ -3,6 +3,6 @@
 // export const socket = io("http://localhost:5000");
 import { io } from "socket.io-client";
 
-export const socket = io("https://chatapps-backend.vercel.app", {
+export const socket = io("https://chatapps-backend.onrender.com", {
   withCredentials: true,
 });
